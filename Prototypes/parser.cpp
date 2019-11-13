66a8c08b0421feb60369ac2b98c7bb1330b59c13
shikhershah038 <sshah038@ucr.edu>
2019-11-12 22:53:13 -0800

    hi

--- Prototypes/parser.cpp
+++ Prototypes/parser.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+
+using namespace std;
+
+
+
 // This function will check the number of occurences of our connector and return it.
 // Our function will take in 2 variables:
 // strCommand: the string that will contain the && and || operators.
 // logicOp: the operator we will be looking for.
 //
 // We will delete the part of the string we have
-int OpOccurence(std::string strCommand, std::string logicOp){
+int OpOccurence(string strCommand, string logicOp){
     int opCounter = 0;
     int location = 0;
     
@@ -23,31 +29,35 @@ int OpOccurence(std::string strCommand, std::string logicOp){
 }
 
 
+
+
+
 int main() {
 
-    std::string user_input = "ls -a;echo hello && echo hi && echo teller || echo fine; ls -m";
+    string user_input = "ls -a;echo hello && echo hi && echo teller || echo fine; ls -m";
     int str_len = user_input.length();
     int connectorLocation;
+   // int quoteLocation;
     int connectorCounter = 0;
-    std::vector<std::string> getCommands;
+
+    vector<string> getCommands;
     
     
-    std::cout << user_input << std::endl;
-    std::cout << std::endl        
+    cout << user_input << endl;
+    cout << endl;        
 
     //Make a while loop that breaks commands based on ; and pushes it to command vector
-    while(user_input.length() != 0){
+    while(str_len != 0){
     // find the location of ; if present in our string.
     // this will tell us were a new command begins
         connectorLocation = user_input.find(";");
 
     // if ; found: get the location of ; then parse and push to our vector command.
     // After delete it from our string
-    if(user_input.find(";") != std::string::npos){
+    if(user_input.find(";") != string::npos){
         connectorLocation = user_input.find(";");
         getCommands.push_back(user_input.substr(0,connectorLocation));
     // else push the remaining code to our vector of commands then delete from our string
-
     } else {
         getCommands.push_back(user_input.substr(0,user_input.length()));
         user_input.erase(0,connectorLocation);
@@ -59,13 +69,14 @@ int main() {
     // if there are more logic operators we will erase what have already compiled and return our next string.
     // We continue doing this until we reach the end of that command line
     int i =0;
+    
     while(i<getCommands.size()){
-        std::cout << "Command[" << i << "]: ";
-        std::cout << getCommands[i] << std::endl;
+        cout << "Command[" << i << "]: ";
+        cout << getCommands[i] << endl;
         i++;
     } 
 
-    std::cout << std::endl;
+    cout << endl;
     // this tests our long command, getCommand[1], and counts the number of logic operators
     // This will tell us how many more comparisons we have.
     // If we have more than one concatanate all the comparison on the left of the logic operator 
@@ -76,15 +87,15 @@ int main() {
     int andCounter = 0;
     int orCounter = 0;
     int total;
-    std::cout << getCommands[1] << std::endl;
-    andCounter = OpOccurence(getCommands[1], "&&");
-    orCounter = OpOccurence(getCommands[1], "||");
+    cout << getCommands[i] << endl;
+    andCounter = OpOccurence(getCommands[i], "&&");
+    orCounter = OpOccurence(getCommands[i], "||");
     total = andCounter + orCounter;
 
-    std::cout << "And counter: " << andCounter << std::endl;
-    std::cout << "Or counter: " << orCounter << std::endl;
-    std::cout << "Total: " << total << std::endl;
-
+    cout << "And counter: " << andCounter << endl;
+    cout << "Or counter: " << orCounter << endl;
+    cout << "Total: " << total << endl;
+	}
 
 
 
