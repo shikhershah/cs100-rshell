d945f41b46b2251daf65bfd37738cf13a309a609
Carlos Loeza <c.loeza1116@gmail.com>
2019-11-12 22:49:57 -0800

    I added comments to parser code

--- Prototypes/parser.cpp
+++ Prototypes/parser.cpp
@@ -50,20 +50,32 @@ int getCommandIndex(string s){
 }
 
 /*
+ *  newString will take in:
+ *  begOp: index of the previous operator encountered
+ *  opIndex: index of the current operator encountered
+ *  s: string of our current command
  *
- *
+ *  we will return a string, which will be a command
+ *  Ex: ls -a
  */
 string newString(int begOp, int opIndex, string s){
+    // if begOp == -1 then we don't have a previous operator
     if(begOp == -1){
+	// if opIndex < s.length() then there must be more commands in our string
         if(opIndex < s.length()){
+	    // get the character of our logic operator using opIndex
 	    char letterOrOP = s.at(opIndex);
+	    // Check to see if we have two consecutive  & or |. This will tell us if we have a valid logic operator
+            // If so, return the string,  beginning to logic index
             if(letterOrOP == '&' && s.at(opIndex+1) == '&')
                 return s.substr(0, opIndex);
             else if(letterOrOP == '|' && s.at(opIndex+1) == '|')
                 return s.substr(0, opIndex);
             else
+		// if no logic operator found return the entire string
                 return s.substr(begOp,s.length());
         }
+    // if we do have a previous logic operator
     } else {
         if(opIndex < s.length()){
             char letterOrOP = s.at(opIndex);
@@ -73,7 +85,7 @@ string newString(int begOp, int opIndex, string s){
                 return s.substr(begOp, opIndex-begOp);
             else
                 return s.substr(begOp, opIndex-begOp);
-        } else if(opIndex == s.length())
+        } else if (opIndex == s.length())
             return  s.substr(begOp,s.length());
         
     }
@@ -81,20 +93,27 @@ string newString(int begOp, int opIndex, string s){
 }
 
 
-
+/*  print will take in:
+ *  s1: string of command1 
+ *  s2: string of command2
+ *
+ *  we will print command1 and command2
+ */
 void print(string s1, string s2){
-    cout << "S1: " << s1 << "   S2: " << s2 << endl;
-    
+    // Check if both are empty   
     if(s1 == " " && s2 == " ")
         cout << "Both strings are empty" << endl;
+    // Check if s1 and s2 have a string
     else if (s1 != " " && s2 != " "){
         cout << "Command 1: " << s1 << endl;
         cout << "Command 2: " << s2 << endl;
     }
+    // Check if s1 has a string and s2 is empty 
     else if(s1 != " " && s2 == " "){
         cout << "Command 1: " << s1 << endl;
         cout << "Command 2: None" << endl;
     }
+    // Check if both s1 and s2 are empty 
     else if (s1 == " " && s1 != " "){
         cout << "Command 1: None" << endl;
         cout << "Command 2: " << s2 << endl;
@@ -104,7 +123,11 @@ void print(string s1, string s2){
 
 
 int main() {
-
+/*
+ *  user_input: string of commands we will parse
+ *  connectorLocation: the location of ";" so we can parse that command
+ *  getCommands: this will hold our commands after we parse our string
+ */
     string user_input = "ls -a;echo \"hello&&\" && echo hi; echo hello && echo hi && echo teller || echo fine || echo last; ls -m";
     int connectorLocation;
     vector<string> getCommands;
@@ -144,6 +167,10 @@ int main() {
 
     cout << endl;
     
+    /*  Our for-loop will iterate through each of our parse commands in getCommands
+     *  test: this will hold our current command
+     *  
+     */
     for(int i = 0; i<getCommands.size(); i++){
         test = getCommands[i];
         string holdString = test;
