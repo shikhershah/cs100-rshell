ea982582dd773bfdad852de4580f9aea4e83024c
Carlos Loeza <c.loeza1116@gmail.com>
2019-11-27 15:23:05 -0800

    I added a default args[] to SingleCommand.h to test our fork and execvp

--- header/Base.h
+++ header/Base.h
@@ -1,6 +1,13 @@
 #ifndef Base_h
 #define Base_h
 
+#include <stdio.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <sys/wait.h> 
+#include <iostream> 
+#include <vector> 
 #include <string>
 using namespace std;
 
--- header/SingleCommand.h
+++ header/SingleCommand.h
@@ -15,11 +15,47 @@ public:
     virtual bool run(){
         if(firstCommand.empty())
             return false;
-	else
+	else{
 	    // test to make sure it works using the forks or execs
+	    char * args[3];
+            string one = "ls";
+            string two = "-a";
+            args[0] = (char*)one.c_str();
+            args[1] = (char*)two.c_str();
+            args[2] = NULL;
+            
+            execute(args);
+
+        }   
         return true;
     }
     
+
+    void execute(char ** args){
+        pid_t pid = fork();
+        int status;
+
+        if(pid < 0){
+            cout << "Error: fork() == -1" << endl;
+            exit(1);
+        }
+        if(pid == 0){
+            cout << "Child: " << pid << endl;
+            if(execvp(args[0], args) ==-1){
+                cout << "Error" << endl;
+                exit(1);
+            }
+        }
+
+        if(pid > 0)
+            waitpid(-1, &pid, 0);
+        cout << "Parent: " << pid << endl;
+
+
+    }
+
+
+
     virtual void print(string firstCommand, string secondCommand){
         if(firstCommand.empty())
             cout << "Error: Missing firstCommand" << endl;
